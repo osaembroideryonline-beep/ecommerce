78bd6a13318ec9380952bad5ece3e1d0200897ab
osaembroideryonline-beep <osaembroideryonline@gmail.com>
2026-01-10 13:31:45 +0530

    Update Introscreen.jsx

--- src/pages/Introscreen.jsx
+++ src/pages/Introscreen.jsx
@@ -23,7 +23,7 @@ const IntroScreen = () => {
         backgroundImage: `url('${
           isPortrait
             ? "https://res.cloudinary.com/dobuwrfn8/image/upload/v1766048280/osalogobg_q33d14.jpg"
-            : "https://res.cloudinary.com/dobuwrfn8/image/upload/v1766046076/osalogobg_hs5rj7.jpg"
+            : "https://res.cloudinary.com/dobuwrfn8/image/upload/v1768031971/osadeskbg_sjgma5.webp"
         }')`,
         backgroundRepeat: "repeat",
         backgroundAttachment: "fixed",
